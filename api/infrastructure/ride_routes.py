f30fcbaab1d9846aa0b7b78cc9d7858072253051
kotnikd3 <denis.kotnik@gmail.com>
2024-07-05 20:19:17 +0200

    Add RIDE_SERVICE to local.env

--- api/infrastructure/ride_routes.py
+++ api/infrastructure/ride_routes.py
@@ -1,22 +1,33 @@
 from typing import Annotated
 
 import httpx
+from decouple import config
 from fastapi import APIRouter, Depends
 
 from api.infrastructure.dependencies import tokens_required
+from api.services.exceptions import ServiceUnavailableException
 
 ride_rooter = APIRouter(prefix='/ride', tags=['ride'])
+
+RIDE_SERVICE = config('RIDE_SERVICE')
 
 
 @ride_rooter.get('/{path:path}')
 async def proxy(path: str, tokens: Annotated[dict, Depends(tokens_required)]):
-    target_url = (
-        f'http://rides:8000/ride/{path}' if path else 'http://rides:8000/ride'
-    )
+    target_url = f'{RIDE_SERVICE}/{path}' if path else RIDE_SERVICE
     headers = {'Authorization': f'Bearer {tokens['access_token']}'}
 
-    # Send request to Rides microservice
-    async with httpx.AsyncClient() as client:
-        response = await client.get(target_url, headers=headers)
+    try:
+        # Send request to Rides microservice
+        async with httpx.AsyncClient() as client:
+            response = await client.get(target_url, headers=headers, timeout=4)
+    except httpx.TimeoutException as error:
+        raise ServiceUnavailableException(
+            f'Ride service is unavailable: {repr(error)}'
+        )
+    except httpx.ConnectError as error:
+        raise ServiceUnavailableException(
+            f'Ride service is unavailable: {repr(error)}'
+        )
 
     return response.json()

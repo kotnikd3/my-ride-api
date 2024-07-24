0a3b10e4a213a79ac7ef3ccaf881c0ed96fc57b1
kotnikd3 <denis.kotnik@gmail.com>
2024-07-24 15:41:18 +0200

    Use test_client instead of async_client

--- tests/api/unit/test_api_routes.py
+++ tests/api/unit/test_api_routes.py
@@ -1,7 +1,5 @@
 from unittest.mock import AsyncMock, patch
 
-import pytest
-
 from api.infrastructure.authentication import KeycloakTokenValidator
 from api.infrastructure.dependencies import COOKIE_NAME, get_tokens
 from api.main import app
@@ -15,26 +13,24 @@ def mocked_tokens():
     }
 
 
-@pytest.mark.asyncio
 @patch.object(KeycloakTokenValidator, 'auth_url', new_callable=AsyncMock)
-async def test_login(mock_keycloak_auth_url, async_client):
+def test_login(mock_keycloak_auth_url, test_client):
     mock_keycloak_auth_url.return_value = '/openid-connect'
 
-    response = await async_client.get(url='/login', follow_redirects=False)
+    response = test_client.get(url='/login', follow_redirects=False)
 
     assert 307 == response.status_code
     assert response.is_redirect
     assert '/openid-connect' in response.headers['location']
 
 
-@pytest.mark.asyncio
 @patch.object(KeycloakTokenValidator, 'logout', new_callable=AsyncMock)
-async def test_logout(mock_keycloak_logout, async_client):
+def test_logout(mock_keycloak_logout, test_client):
     mock_keycloak_logout.return_value = None
     app.dependency_overrides[get_tokens] = mocked_tokens
 
-    async_client.cookies[COOKIE_NAME] = 'mocked'
-    response = await async_client.get(
+    test_client.cookies[COOKIE_NAME] = 'mocked'
+    response = test_client.get(
         '/logout?frontend_uri=https://frontend', follow_redirects=False
     )
 
@@ -44,12 +40,11 @@ async def test_logout(mock_keycloak_logout, async_client):
     assert COOKIE_NAME not in response.cookies
 
 
-@pytest.mark.asyncio
 @patch.object(KeycloakTokenValidator, 'get_tokens', new_callable=AsyncMock)
-async def test_authorize(mock_keycloak_get_tokens, async_client):
+def test_authorize(mock_keycloak_get_tokens, test_client):
     mock_keycloak_get_tokens.return_value = mocked_tokens()
 
-    response = await async_client.get(
+    response = test_client.get(
         url='/authorize?frontend_uri=https://frontend',
         params={'code': 'some_code'},
         follow_redirects=False,
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,12 @@
-from typing import AsyncGenerator
-
 import pytest
-from httpx import ASGITransport, AsyncClient
+from fastapi.testclient import TestClient
 
 from api.main import app
 
 
 @pytest.fixture(scope='function')
-async def async_client() -> AsyncGenerator[AsyncClient, None]:
+def test_client():
     app.dependency_overrides = {}
-    transport = ASGITransport(app=app)
 
-    async with AsyncClient(
-        transport=transport, base_url='http://testserver'
-    ) as client:
-        yield client
+    with TestClient(app) as c:
+        yield c
